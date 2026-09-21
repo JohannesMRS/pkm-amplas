741c8e4c6ef380706c01a05dc8622276ff080ae0
JohannesMRS <mariojr8304@gmail.com>
2026-09-21 21:30:35 +0700

    feat: adding product controller

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,16 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
+    protected $fillable = [
+        'id',
+        'name', // jenis layanan (cuci kering, )
+        'description',
+        'price',
+        'unit',
+        'is_active'
+
+    ];
+    
     public function orderDetails():HasMany{
         return $this->hasMany(OrderDetail::class);
     }
